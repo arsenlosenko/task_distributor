5f601c0aa6c1bbf5f0d1a0b804817d3b06c77b8c
Arsen Losenko <arsenlosenko@gmail.com>
2017-03-28 19:55:22 +0300

    let => var

--- public/front.js
+++ public/front.js
@@ -14,19 +14,19 @@
  
 
  function createTask(){
-    let taskName = document.getElementById('taskName');
-    let taskDescription = document.getElementById('taskDescription');
-    let taskDeadline = document.getElementById('taskDeadline');
+    var taskName = document.getElementById('taskName');
+    var taskDescription = document.getElementById('taskDescription');
+    var taskDeadline = document.getElementById('taskDeadline');
     console.log('Task name is ' + taskName.value + ' description is ' + taskDescription.value + ' and its due to ' + taskDeadline.value);
     return false;
 }
 
 function createEl(){
-    let textNode = prompt('Add text: ');
-    let container = document.getElementById('tasks');
-    let paragraph = document.createElement('p');
-    let anchor = document.createElement('a');
-    let text = document.createTextNode(textNode);
+    var textNode = prompt('Add text: ');
+    var container = document.getElementById('tasks');
+    var paragraph = document.createElement('p');
+    var anchor = document.createElement('a');
+    var text = document.createTextNode(textNode);
     paragraph.appendChild(anchor);
     anchor.appendChild(text);       
     container.appendChild(paragraph);
@@ -35,8 +35,8 @@ function createEl(){
 //скрипт для тесту переходу на індекс
 // function changePage(){
 //     window.location.href = "../views/index.html";
-//     let email = document.getElementById('emailInput');
-//     let pass = document.getElementById('passwordInput');
+//     var email = document.getElementById('emailInput');
+//     var pass = document.getElementById('passwordInput');
 //     console.log('Login is ' + email.value + ' and password is ' + pass.value);
 //     return false;
 
